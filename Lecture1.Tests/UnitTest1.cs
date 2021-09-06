926fe40bf143dec62b18ddf204d044fee0e0cdeb
niwl <niwl@itu.dk>
2021-09-06 16:40:47 +0200

    Tested and implemented isLeapYear

--- Lecture1.Tests/UnitTest1.cs
+++ Lecture1.Tests/UnitTest1.cs
@@ -1,14 +1,38 @@
 using System;
 using Xunit;
+using Lecture1;
 
 namespace Lecture1.Tests
 {
     public class UnitTest1
     {
+        
         [Fact]
-        public void Test1()
-        {
+        public void Test_LeapYear_given_4_returns_true(){
+            var expected = true;
+            
+            var actual = new Program().LeapYear2(4);
 
+            Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void Test_LeapYear_given_1600_returns_true()
+        {
+            var expected = true;
+
+            var actual = new Program().LeapYear3(1600);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test_LeapYear_given_1700_false(){
+            var expected = false;
+
+            var actual = new Program().IsLeapYear(1700);
+
+            Assert.Equal(expected,actual);
         }
     }
 }
--- Lecture1/Program.cs
+++ Lecture1/Program.cs
@@ -2,11 +2,50 @@
 
 namespace Lecture1
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+
+            Program program = new Program();
+            Console.WriteLine(program.IsLeapYear(12));
+            Console.WriteLine(program.IsLeapYear(1600));
+            Console.WriteLine(program.IsLeapYear(1700));
+        }
+
+        public bool LeapYear2(int year){
+            if(year%4 == 0){
+                return true;
+            } else{
+                return false;
+            }
+        }
+
+        public bool LeapYear3(int year){
+            if(year%100 == 0){
+                    if(year%400 ==0){  
+                            return true;
+                        } else {
+                           
+                            return false;
+                        }
+                }
+                return true;
+        }
+        public bool IsLeapYear(int year){
+            if(year%4 == 0){
+                if(year%100 == 0){
+                    if(year%400 ==0){  
+                            return true;
+                        } else {
+                           
+                            return false;
+                        }
+                }
+                return true;
+            }
+            return false;
         }
     }
 }
