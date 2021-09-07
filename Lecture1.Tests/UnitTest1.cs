c4b2dd86b072f6eac7cf0992d6b3405feb7d1924
NikolajWLarsen <90180988+NikolajWLarsen@users.noreply.github.com>
2021-09-07 18:11:20 +0200

    Leap year (#2)
    * Tested and implemented isLeapYear
    
    * Created user interface for IsLeapYear
    
    * implemented error handling
    
    Co-authored-by: niwl <niwl@itu.dk>

--- Lecture1.Tests/UnitTest1.cs
+++ Lecture1.Tests/UnitTest1.cs
@@ -1,14 +1,52 @@
 using System;
 using Xunit;
+using Lecture1;
 
 namespace Lecture1.Tests
 {
     public class UnitTest1
     {
+        
         [Fact]
-        public void Test1()
+        public void Test_isLeapYear_given_2000_returns_true(){
+            var expected = true;
+            
+            var actual = new Program().IsLeapYear(2000);
+
+            Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void Test_isLeapYear_given_1600_returns_true()
         {
+            var expected = true;
+
+            var actual = new Program().IsLeapYear(1600);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test_isLeapYear_given_1700_false(){
+            var expected = false;
+
+            var actual = new Program().IsLeapYear(1700);
+
+            Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void Test_throw_exception_if_year_is_less_than_1582(){
+            
+            Assert.Throws<Exception>( () => new Program().IsLeapYear(1580));
 
         }
+
+
+        /*public void test_if_user_gives_a_string(){
+            var expected = 
+
+            Assert.Throws<Exception>( () => new Program().readInput());
+        }*/
     }
 }
--- Lecture1/Program.cs
+++ Lecture1/Program.cs
@@ -2,11 +2,43 @@
 
 namespace Lecture1
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program program = new Program();
+            
+            int input = program.readInput();
+            if (program.IsLeapYear(input) == true){
+                Console.WriteLine("yay");
+            } else {
+                Console.WriteLine("ney");
+            }
+        }
+        public int readInput(){
+            try{
+                return int.Parse(Console.ReadLine());
+            } catch{
+                throw new Exception("Input is not an integer");
+            }
+        }
+
+        public bool IsLeapYear(int year){
+            if(year<1582){
+                throw new Exception();  //Only check Leap years from 1583 and above. 
+            }
+            if(year%4 == 0){
+                if(year%100 == 0){
+                    if(year%400 ==0){  
+                            return true;
+                        } else {
+                           
+                            return false;
+                        }
+                }
+                return true;
+            }
+            return false;
         }
     }
 }
